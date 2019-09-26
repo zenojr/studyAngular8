f889585ff5169e6782f1b4eca83328ce7343346c
zenojr <zfrancajunior@gmail.com>
2019-09-26 09:38:41 -0300

    update proj

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,4 @@
-import { HEROES } from './../mock-heroes';
+import { HeroService } from './../hero.service';
 import { Hero } from './../hero';
 import { Component, OnInit } from '@angular/core';
 
@@ -9,17 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Zeno'
-  };
+  // hero: Hero = {
+  //   id: 1,
+  //   name: 'Zeno'
+  // };
 
-  heroes = HEROES;
+  heroes: Hero[];
+
   selectedHero: Hero;
 
-  constructor() { }
+  constructor( private heroService: HeroService ) { }
 
   ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
   }
 
   onSelect(hero: Hero): void {
