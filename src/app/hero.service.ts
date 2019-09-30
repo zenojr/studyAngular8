1e74713aa03da0b5a0213c5530f7784c60289c8b
zenojr <zfrancajunior@gmail.com>
2019-09-30 17:25:07 -0300

    update project

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -35,7 +35,19 @@ export class HeroService {
     );
   }
 
-  addHero(hero: Hero): Observable<Hero>{
+
+  deleteHero(hero: Hero | number): Observable<Hero> {
+    const id = typeof hero === 'number' ? hero : hero.id;
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+      tap( _ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deletehero'))
+    );
+  }
+
+  /** POST: add a new hero to the server */
+  addHero(hero: Hero): Observable<Hero> {
     return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
       tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
       catchError(this.handleError<Hero>('addHero'))
